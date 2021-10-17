4eb6dc712cb656a8dbd5d6ded831e2301228d36b
Tanvi Kshatriya <punamtanvi.kshatriya18@vit.edu>
2021-10-17 16:21:11 +0530

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+//react-router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//components
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+import {UserContext} from "./context/UserContext"
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+
+//firebase 
+import firebase from 'firebase/compat';
+
+import firebaseConfig from './config/firebaseConfig';
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{user, setUser}}>
+        <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="*" component={PageNotFound} />
+          </Switch>
+          <Footer />
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDe3kbqXktIp1BdAjBRODEm2U3mx6vsBlI",
+    authDomain: "mygitappp.firebaseapp.com",
+    projectId: "mygitappp",
+    storageBucket: "mygitappp.appspot.com",
+    messagingSenderId: "619830178822",
+    appId: "1:619830178822:web:867e8065bb03f4baefff87",
+    measurementId: "G-FNB9EZ2QZD"
+  };
